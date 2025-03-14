46f1750c87038290ded4110cb7556155513346d5
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2025-03-13 22:27:30 -0600

    Add files via upload

---
+++ Alsuper_PickUp/src/main/java/com/mycompany/alsuper_pickup/Mascotas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.alsuper_pickup;
+
+/**
+ *
+ * @author aaro1
+ */
+public class Mascotas {
+    
+}
